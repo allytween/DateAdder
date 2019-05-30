fa14206e1a349a44b5f94d5c4eaa261e1b21174d
allytween <38024580+allytween@users.noreply.github.com>
2019-05-30 09:54:59 -0400

    Add files via upload

---
+++ DateAdder.java
@@ -0,0 +1,11 @@
+// Date and Time Program
+// M. Tween
+// 5/9/19
+// This program displays todays date and the sum of yesterday and today’s dates
+
+public class DateAdder {
+   public static void main(String[] args){
+      System.out.println("Today's date is 5/9/19");
+      System.out.println("The sum of today and yesterday's date is " + (8+9));
+      }
+}
